25b1a39247e8db15f770fed775c74cba5dca9321
Jingxuan <zhangjingxuan0323@gmail.com>
2024-10-13 00:21:18 +0200

    barrier placement and movement

--- CatGame.java
+++ CatGame.java
@@ -7,10 +7,13 @@ public class CatGame extends JPanel implements ActionListener, KeyListener{
     int panelWidth = 750;
     int panelHeight = 250;
     Image catImg;
+    Image barrierImg;
 
     Timer timer;
+    Timer barrierTimer;
     int jumpVelocity;
     int gravity = 1;
+    int barrierVelocity = -12;
     boolean jumping = false;
 
     class Item {
@@ -37,6 +40,14 @@ public class CatGame extends JPanel implements ActionListener, KeyListener{
     int catY = panelHeight - catHeight;
     Item cat;
 
+    //barrier
+    int barrierWidth = 34;
+    int barrierHeight = 70;
+    int barrierX = 700;
+    int barrierY = panelHeight - barrierHeight;
+
+    ArrayList<Item> barrierArray;
+
     public CatGame(){
         setPreferredSize(new Dimension(panelWidth, panelHeight));
         setBackground(Color.lightGray);
@@ -46,21 +57,42 @@ public class CatGame extends JPanel implements ActionListener, KeyListener{
         //render inmage
 
         catImg = new ImageIcon(getClass().getResource("/img/dino-run.gif")).getImage();
+        barrierImg = new ImageIcon(getClass().getResource("/img/cactus1.png")).getImage();
+        barrierArray = new ArrayList<Item>();
 
         cat = new Item(catX, catY, catWidth, catHeight, catImg);
 
         //game loop
         timer = new Timer(1000/60, this);
         timer.start();
+
+        barrierTimer = new Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                generateBarrier();
+            }
+        });
+        barrierTimer.start();
     }
+
+    void generateBarrier() {
+        Item barrier = new Item(barrierX, barrierY, barrierWidth, barrierHeight, barrierImg);
+        barrierArray.add(barrier);
+    }
+
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         draw(g);
     }
 
     public void draw(Graphics g){
-        //cat
+        //draw cat
         g.drawImage(cat.img, cat.x, cat.y, cat.width, cat.height, null);
+        //draw barrier
+        for (int i = 0; i < barrierArray.size(); i++){
+            Item barrier = barrierArray.get(i);
+            g.drawImage(barrier.img, barrier.x, barrier.y, barrier.width, barrier.height, null);
+        }
     }
 
     public void move(){
@@ -73,6 +105,16 @@ public class CatGame extends JPanel implements ActionListener, KeyListener{
                 jumping = false;
             }
 
+        }
+        //barrier move
+        for (int i = 0; i < barrierArray.size(); i++){
+            Item barrier = barrierArray.get(i);
+            barrier.x += barrierVelocity;
+
+            if (barrier.x + barrier.width < 0) { //if move outside screen
+                barrierArray.remove(i);
+                i--; //adjust index
+            }
         }
     }
 
