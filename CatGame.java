39f19992993309f85181439bbc03974b14ec70f3
Jingxuan <zhangjingxuan0323@gmail.com>
2024-10-12 22:30:05 +0200

    draw sprite

--- CatGame.java
+++ CatGame.java
@@ -6,10 +6,48 @@ import javax.swing.*;
 public class CatGame extends JPanel {
     int panelWidth = 750;
     int panelHeight = 250;
+    Image catImg;
+
+    class Item {
+        int x;
+        int y;
+        int width;
+        int height;
+        Image img;
+
+        Item(int x, int y, int width, int height, Image img) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.img = img;
+
+        }
+    }
+
+    //cat
+    int catWidth = 88;
+    int catHeight = 94;
+    int catX = 50;
+    int catY = panelHeight - catHeight;
+    Item cat;
 
     public CatGame(){
         setPreferredSize(new Dimension(panelWidth, panelHeight));
         setBackground(Color.lightGray);
+
+        catImg = new ImageIcon(getClass().getResource("/img/dino.png")).getImage();
+
+        cat = new Item(catX, catY, catWidth, catHeight, catImg);
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+        //cat
+        g.drawImage(cat.img, cat.x, cat.y, cat.width, cat.height, null);
     }
  
 }
